789e9984244618482f99d2dd8cdd4a79f8ec5f33
hulk19 <phamhungbkhn@gmail.com>
2023-11-23 18:24:35 +0700

    init: frontend

---
+++ exercises/lesson-9/front-end/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Providers from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sui Bootcamp - VBI 2024",
+  description: "Sui Bootcamp - VBI Academy 2024",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <NavBar />
+          <div className="pt-[70px]">
+            {children}
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
